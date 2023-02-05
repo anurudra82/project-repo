6df9d4276f58f7455942932dff8f9a5c9c53a3fa
Chandra Pal Singh Chauhan <95373151+anurudra82@users.noreply.github.com>
2023-02-06 03:04:46 +0530

    Add files via upload

---
+++ otp_generator.py
@@ -0,0 +1,26 @@
+import math
+from random import *
+ 
+
+def gen_OTP() :
+
+    
+    digits = "0123456789"
+    OTP = ""
+ 
+   # length of password can be changed
+   # by changing value in range
+    print("your 6 digit otp: ",end="")
+    for i in range(6) :
+        OTP = str(randint(0,9)) + OTP
+
+    
+        
+      
+        
+        
+        # OTP = digit[math.floor(random.random()*10] + OTP
+ 
+    return OTP
+     
+print(gen_OTP())
